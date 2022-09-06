da16800a4371a1f1e4d6f1391025becc88ac23b0
b4p3d1 <mokgotho66@outlook.com>
2022-09-06 23:03:43 +0200

    name update

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ impl event::EventHandler<ggez::GameError> for Game {
 
 pub fn main() -> GameResult {
     // Create a game context and event loop
-    let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
-        .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
+    let context_builder = ggez::ContextBuilder::new("susa", "susa")
+        .window_setup(conf::WindowSetup::default().title("Susa!"))
         .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
         .add_resource_path(path::PathBuf::from("./resources"));
 
